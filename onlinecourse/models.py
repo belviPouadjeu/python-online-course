035bda0b8762800ed4709022614f58dbc85f4169
belviPouadjeu <pbelvinard21@gmail.com>
2025-09-03 11:44:56 +0000

    Register Model Changes

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -125,14 +125,3 @@ class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
 
-class ChoiceInline(admin.StackedInline):
-    model = Choice
-    extra = 2
-
-class QuestionInline(admin.StackedInline):
-    model = Question
-    extra = 2
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-    list_display = ['content']
